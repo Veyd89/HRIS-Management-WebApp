33da0a81607026db9394bf3db342de1b45b3029c
Dayen <veydayenfatzla@gmail.com>
2025-03-29 12:12:19 +0700

    progress loginPage

--- src/pages/LoginPages.jsx
+++ src/pages/LoginPages.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+// import Screenshot from "../Screenshot.png";
 
 const LoginPages = () => {
   return (
-    <section className="bg-gradient-to-tr from-[#DCE5F2] to-[#F1F4F9] h-screen w-full flex justify-center items-center overflow-hidden ">
-      <img src="Screenshot.png" className=" w-48 h-48" alt="" />
-      {/* <img src="../assets/Y.png" className="absolute w-48 h-48" alt="" /> */}
+    <section className="bg-gradient-to-tr from-[#DCE5F2] to-[#F1F4F9] relative h-screen w-full flex justify-center items-center overflow-hidden ">
+      <img
+        src="/img/X.png"
+        className="absolute  w-[1000px] h-[400px] rotate-[52.34deg] top-[17rem] "
+        alt=""
+      />
+      <img
+        src="/img/Y.png"
+        className="absolute  w-[1100px] h-[400px] -rotate-[7deg] top-40"
+        alt=""
+      />
+      <div className="w-[750px] h-[500px] z-50 bg-[#E6EEF8] py-16 pl-[70px] pr-20 shadow-[-16px_-16px_40px_rgba(253,255,255,0.8),16px_16px_40px_rgba(187,195,206,0.6)]">
+        <div className="min-w-full min-h-full flex flex-col">
+          <header className="flex w-full justify-center items-center">
+            <img src="/img/mainLogo.png" alt="" />
+            <h1>test</h1>
+          </header>
+        </div>
+      </div>
     </section>
   );
 };

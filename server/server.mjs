d8525541db13f215f03ba8eced40072a0e2ccedc
Dayen Al Fiansyach <veydayenfatzla@gmail.com>
2025-04-29 23:07:41 +0700

    progress add server.mjs

---
+++ server/server.mjs
@@ -0,0 +1,34 @@
+import jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+// Middleware default (logger, CORS, dll)
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+
+// Untuk menangani POST /login secara custom
+server.post("/login", (req, res) => {
+  const responseAll = req.body;
+  const { email, password } = responseAll;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  const users = router.db.get("users").value();
+  const user = users.find((u) => u.email === email && u.password === password);
+  if (user) {
+    res.status(200).json({ user });
+  } else {
+    res.status(400).json({ message: "Invalid email or password" });
+  }
+});
+
+server.use(router);
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running at http://localhost:${PORT} `);
+});

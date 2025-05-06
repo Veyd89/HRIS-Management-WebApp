16a89e5b59e5e8e6e5d391d647369c1f3ad9d52f
Dayen <veydayenfatzla@gmail.com>
2025-05-06 23:14:41 +0700

    s

--- src/components/elements/Sidebar.jsx
+++ src/components/elements/Sidebar.jsx
@@ -15,25 +15,25 @@ const Sidebar = () => {
       <nav className="h-full bg-[#082777] flex flex-col items-center px-4 pt-7 gap-24">
         <img src="/img/mainLogo.png" className="w-7 h-12" alt="" />
         <ul className="flex flex-col gap-7">
-          <li className="cursor-pointer flex flex-col  items-center text-[#E6EEF8] ">
-            <FaHome className="w-10 h-12" />
-            <span className="text-lg -mt-0">home</span>
+          <li className="cursor-pointer gap-1 flex flex-col  items-center text-[#E6EEF8] ">
+            <FaHome className="text-4xl" />
+            <span className="text-lg ">home</span>
           </li>
-          <li className="cursor-pointer flex flex-col  items-center text-[#E6EEF8] ">
-            <FaBagShopping className="w-10 h-12" />
-            <span className="text-lg -mt-0">jobs</span>
+          <li className="cursor-pointer gap-1 flex flex-col  items-center text-[#E6EEF8] ">
+            <FaBagShopping className="text-4xl" />
+            <span className="text-lg ">jobs</span>
           </li>
-          <li className="cursor-pointer flex flex-col  items-center text-[#E6EEF8] ">
-            <FaUserGroup className="w-10 h-12" />
-            <span className="text-lg -mt-0">candidates</span>
+          <li className="cursor-pointer gap-1 flex flex-col  items-center text-[#E6EEF8] ">
+            <FaUserGroup className="text-4xl" />
+            <span className="text-lg ">candidates</span>
           </li>
-          <li className="cursor-pointer flex flex-col  items-center text-[#E6EEF8] ">
-            <FaFile className="w-10 h-12" />
-            <span className="text-lg -mt-0">reports</span>
+          <li className="cursor-pointer gap-1 flex flex-col  items-center text-[#E6EEF8] ">
+            <FaFile className="text-4xl" />
+            <span className="text-lg ">reports</span>
           </li>
-          <li className="cursor-pointer flex flex-col  items-center text-[#E6EEF8] ">
-            <FaCalendar className="w-10 h-12" />
-            <span className="text-lg -mt-0">calendar</span>
+          <li className="cursor-pointer gap-1 flex flex-col  items-center text-[#E6EEF8] ">
+            <FaCalendar className="text-4xl" />
+            <span className="text-lg ">calendar</span>
           </li>
         </ul>
       </nav>

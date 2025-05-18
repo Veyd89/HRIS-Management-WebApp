f44c3ca08365f66ba8829d79620a941b7e4ac777
Dayen Al Fiansyach <veydayenfatzla@gmail.com>
2025-05-18 23:12:41 +0700

    header home

--- src/components/elements/HeaderHome.jsx
+++ src/components/elements/HeaderHome.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { IoIosSearch } from "react-icons/io";
+import { IoIosArrowDown, IoIosSearch } from "react-icons/io";
 
 const HeaderHome = () => {
   return (
-    <header className=" top-0 h-24 sticky bg-white pl-40 pr-4 py-6">
-      <div className="flex ">
-        <div className="relative opacity-40 w-[400px]">
+    <header className=" top-0 h-20 sticky bg-white pl-40 pr-9 flex items-center">
+      <div className="flex justify-between items-center w-full">
+        <div className="relative flex justify-between opacity-40 w-[400px]">
           <input
             placeholder="Search"
             type="text"
             className="w-full text-lg rounded-lg py-2 pl-12 pr-2 outline-none border border-[#082777] border-opacity-[24%] bg-[#E5EDF9] bg-opacity-40"
           />
           <IoIosSearch className="absolute top-[25%] text-2xl left-4" />
+        </div>
+        <div className="flex items-center gap-4 font-semibold">
+          <img
+            src="/img/naruto.jpg"
+            alt=""
+            className="w-11 h-11 object-cover rounded-full border-2 border-[#000000] "
+          />
+          <span className="flex items-center gap-2">
+            Naruto{" "}
+            <IoIosArrowDown className="text-lg text-[#071C50] opacity-50" />
+          </span>
         </div>
       </div>
     </header>
--- src/components/elements/Sidebar.jsx
+++ src/components/elements/Sidebar.jsx
@@ -13,27 +13,27 @@ const Sidebar = () => {
   return (
     <aside className="h-screen w-32 z-50 fixed top-0">
       <nav className="h-full bg-[#082777] flex flex-col items-center px-4 pt-7 gap-24 rounded-r-lg">
-        <img src="/img/mainLogo.png" className="w-7 h-12" alt="" />
+        <img src="/img/mainLogo.png" className="w-6 h-10" alt="" />
         <ul className="flex flex-col gap-7">
           <li className="cursor-pointer gap-1 flex flex-col  items-center text-[#E6EEF8] ">
-            <FaHome className="text-4xl" />
-            <span className="text-lg ">home</span>
+            <FaHome className="text-3xl" />
+            <span className="text-base font-semibold">home</span>
           </li>
           <li className="cursor-pointer gap-1 flex flex-col  items-center text-[#E6EEF8] opacity-40 ">
-            <FaBagShopping className="text-4xl" />
-            <span className="text-lg ">jobs</span>
+            <FaBagShopping className="text-3xl" />
+            <span className="text-base font-semibold ">jobs</span>
           </li>
           <li className="cursor-pointer gap-1 flex flex-col  items-center text-[#E6EEF8] opacity-40 ">
-            <FaUserGroup className="text-4xl" />
-            <span className="text-lg ">candidates</span>
+            <FaUserGroup className="text-3xl" />
+            <span className="text-base font-semibold ">candidates</span>
           </li>
           <li className="cursor-pointer gap-1 flex flex-col  items-center text-[#E6EEF8] opacity-40 ">
-            <FaFile className="text-4xl" />
-            <span className="text-lg ">reports</span>
+            <FaFile className="text-3xl" />
+            <span className="text-base font-semibold ">reports</span>
           </li>
           <li className="cursor-pointer gap-1 flex flex-col  items-center text-[#E6EEF8] opacity-40 ">
-            <FaCalendar className="text-4xl" />
-            <span className="text-lg ">calendar</span>
+            <FaCalendar className="text-3xl" />
+            <span className="text-base font-semibold ">calendar</span>
           </li>
         </ul>
       </nav>

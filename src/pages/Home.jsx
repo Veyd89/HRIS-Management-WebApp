89d9740f7db8d58a5f9437ee5d4acd788b840877
Dayen <veydayenfatzla@gmail.com>
2025-05-29 16:52:53 +0700

    m

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,17 @@ const Home = () => {
     <section className="min-h-[10000px] ">
       <HeaderHome />
       <Sidebar />
+      <div className="pl-[155px] pr-[350px] ">
+        <div className="pl-7">
+          <div className="flex justify-between">
+            <h3>Overview</h3>
+            <div className="flex gap-4">
+              <button>Add</button>
+              <button>Add</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
